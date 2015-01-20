4697054767fcdf04f355ebd236a0d1d1ef5b142e
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2015-01-20 16:41:37 +0100

    Initial commit of defeatureify

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-/* jshint node: true */
+/* jshint node: true, sub: true */
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-defeatureify'
+  name: 'ember-cli-defeatureify',
+  included: function(app) {
+    console.log('Included defeatureify plugin');
+    this._super.included.apply(this, arguments);
+
+    var options = getOptions(app, app.options['defeatureify']);
+
+    var plugin = {
+      name: 'defeatureify',
+      ext: 'js',
+      toTree: function(tree) {
+        return require('broccoli-defeatureify')(tree, options);
+      }
+    };
+
+    app.registry.add('js', plugin);
+  }
 };
+
+function getOptions(app, options) {
+  options = options || {};
+
+  if(!options.namespace) {
+    options.namespace = app.name;
+  }
+
+  if(!options.features) {
+    throw new Error("Features are not specified");
+  }
+
+  return options;
+}
