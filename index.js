441a537ace9889baf11e2d35ff100b2e521d8f3e
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2015-01-23 11:06:17 +0100

    Don't register as a plugin, and run defeatureify in postprocessTree

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 /* jshint node: true, sub: true */
 'use strict';
 
+var defeatureify = require('broccoli-defeatureify');
+
 module.exports = {
   name: 'ember-cli-defeatureify',
   included: function(app) {
     this._super.included.apply(this, arguments);
-
-    var options = getOptions(app, app.options['defeatureify']);
-
-    var plugin = {
-      name: 'ember-cli-defeatureify',
-      ext: 'js',
-      toTree: function(tree) {
-        return require('broccoli-defeatureify')(tree, options);
-      }
-    };
-
-    app.registry.add('js', plugin);
+    this.app = app;
+    this.options = getOptions(app, app.options['defeatureify']);
+  },
+  postprocessTree: function(type, tree) {
+    if(this.app.env === 'production' && type === 'all') {
+      tree = defeatureify(tree, this.options);
+    }
+    return tree;
   }
 };
 
