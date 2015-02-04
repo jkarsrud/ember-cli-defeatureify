1b79a6c9328d4b734913b18bd983ec5e520c1d13
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2015-02-04 10:37:03 +0100

    Test generation of feature scripts

---
+++ tests/insert-features-test.js
@@ -0,0 +1,32 @@
+var insertFeatures = require('../lib/insert-features');
+
+exports.testInsertFeaturesWithOneFeature = function(test) {
+  var expected = '<script>TEST = {};TEST.FEATURES = {"test":true};TEST.FEATURES.isEnabled = function(feature) {return this[feature];};</script>';
+  var options = {
+    namespace: 'TEST',
+    features: {
+      test: true
+    }
+  };
+
+  var actual = insertFeatures(options);
+
+  test.equal(actual, expected);
+  test.done();
+};
+
+exports.testInsertFeaturesWithMultipleFeatures = function(test) {
+  var expected = '<script>TEST = {};TEST.FEATURES = {"test":true,"anotherFeature":false};TEST.FEATURES.isEnabled = function(feature) {return this[feature];};</script>';
+  var options = {
+    namespace: 'TEST',
+    features: {
+      test: true,
+      anotherFeature: false
+    }
+  };
+
+  var actual = insertFeatures(options);
+
+  test.equal(actual, expected);
+  test.done();
+};
